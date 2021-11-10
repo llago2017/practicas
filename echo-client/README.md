11dac104343f1c7b20a1ad6c56e976356f1e9082
Lorena <l.lago.2017@alumnos.urjc.es>
2021-11-10 14:37:47 +0100

    Creación de cliente

---
+++ echo-client/README.md
@@ -0,0 +1,3 @@
+# echo-client
+
+A description of this package.
---
+++ echo-client/Sources/echo-client/main.swift
@@ -0,0 +1,18 @@
+import Socket
+import Glibc
+
+let server = "localhost"
+let port: Int32 = 7667
+let message = "Hello, world!"
+
+do {
+    guard let serverAddress = Socket.createAddress(for: server, on: port) else {
+        print("Error creating Address")
+        exit(1)
+    }
+
+    let clientSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
+    try clientSocket.write(from: message, to: serverAddress)
+} catch let error {
+    print("Connection error: \(error)")
+}
