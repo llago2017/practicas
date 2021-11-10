be8c5a9737709ac239e40098e5a874ab25dc85bc
Lorena <l.lago.2017@alumnos.urjc.es>
2021-11-10 14:46:22 +0100

    Modiciación del cliente v1

--- echo-client/Sources/echo-client/main.swift
+++ echo-client/Sources/echo-client/main.swift
@@ -1,18 +1,28 @@
 import Socket
 import Glibc
 
-let server = "localhost"
-let port: Int32 = 7667
 let message = "Hello, world!"
 
-do {
-    guard let serverAddress = Socket.createAddress(for: server, on: port) else {
-        print("Error creating Address")
-        exit(1)
+var arguments = CommandLine.arguments
+if !arguments.isEmpty {
+    if arguments.count > 1 {
+        let server = arguments[1]
+        let port = Int32(arguments[2])!
     }
 
-    let clientSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
-    try clientSocket.write(from: message, to: serverAddress)
-} catch let error {
-    print("Connection error: \(error)")
+    do {
+        guard let serverAddress = Socket.createAddress(for: arguments[1], on: Int32(arguments[2])!) else {
+            print("Error creating Address")
+            exit(1)
+        }
+
+        let clientSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
+        try clientSocket.write(from: message, to: serverAddress)
+    } catch let error {
+        print("Connection error: \(error)")
+    }
+    
 }
+
+
+
