2cc0ea86329df368985af6695f0d81225d24ecce
Lorena <l.lago.2017@alumnos.urjc.es>
2021-11-10 14:50:05 +0100

    Mejora modificación, recibe argumentos bien

--- echo-client/Sources/echo-client/main.swift
+++ echo-client/Sources/echo-client/main.swift
@@ -5,22 +5,28 @@ let message = "Hello, world!"
 
 var arguments = CommandLine.arguments
 if !arguments.isEmpty {
-    if arguments.count > 1 {
+    if arguments.count != 3 {
+        print("Inicie con: swift run <echo-client> <server-name> <port>")
+        
+    } else {
         let server = arguments[1]
         let port = Int32(arguments[2])!
-    }
 
-    do {
-        guard let serverAddress = Socket.createAddress(for: arguments[1], on: Int32(arguments[2])!) else {
-            print("Error creating Address")
-            exit(1)
+         do {
+            guard let serverAddress = Socket.createAddress(for: server, on: port) else {
+                print("Error creating Address")
+                exit(1)
+            }
+
+            let clientSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
+            try clientSocket.write(from: message, to: serverAddress)
+        } catch let error {
+            print("Connection error: \(error)")
         }
-
-        let clientSocket = try Socket.create(family: .inet, type: .datagram, proto: .udp)
-        try clientSocket.write(from: message, to: serverAddress)
-    } catch let error {
-        print("Connection error: \(error)")
+        
     }
+
+   
     
 }
 
